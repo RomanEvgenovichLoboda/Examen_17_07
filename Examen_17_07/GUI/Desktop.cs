51ae3533ee0cd6af36497408fcc8ff8a94d6dfbc
loboda.rom@gmail.com <loboda.rom@gmail.com>
2022-07-22 13:19:25 +0300

    add_while

--- Examen_17_07/GUI/Desktop.cs
+++ Examen_17_07/GUI/Desktop.cs
@@ -1,12 +1,15 @@
-﻿using Examen_17_07.GUI;
+﻿//using ParrentConsole;
+using Examen_17_07.GUI;
 using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,6 +17,8 @@ namespace Examen_17_07
 {
     public partial class Desktop : Form
     {
+         List<string> list_names = new List<string>();
+        //public Timer timer = new Timer();
         public Desktop()
         {
             InitializeComponent();
@@ -23,7 +28,27 @@ namespace Examen_17_07
             //this.WindowState = FormWindowState.Maximized;
             //this.FormBorderStyle = FormBorderStyle.None;
             //Get_Icons();
+            //Program2 program2 = new Program2();
+
+
+            //timer.Interval = 100;
+            //timer.Tick += (async(s, e) => ProcessMonitor());
+            //timer.Start();
+
+           // TimerStart();
         }
+
+        // async void TimerStart()
+        //{
+        //    await Task.Run(async () =>
+        //    {
+        //        Timer timer = new Timer();
+        //        timer.Interval = 100;
+        //        timer.Tick += ((s, e) => ProcessMonitor());
+        //        timer.Start();
+        //        await Task.Delay(1);
+        //    });
+        //}
 
         private void Desktop_Load(object sender, EventArgs e)
         {
@@ -31,62 +56,105 @@ namespace Examen_17_07
             passForm.ShowDialog();
             if (!passForm.varif_Ok) { this.Close(); }
         }
-        private void Get_Icons()
-        {
-            int x = 0, y = 0;
-            using (RegistryKey reg_key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"))
-            //using (RegistryKey reg_key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"))
-            {
-                string[] app_names = reg_key.GetSubKeyNames();
-                foreach (string name in app_names)
-                {
 
-                    string icon_path = null;
-                    string icon_name = null;
-                    using (RegistryKey app_key = reg_key.OpenSubKey(name))
-                    {
-                        icon_path = app_key?.GetValue("DisplayIcon")?.ToString();
-                        icon_name = app_key?.GetValue("DisplayName")?.ToString();
-                    }
-                    if (icon_path != null)
-                    {
-                        try
-                        {
-                            Image img = Icon.ExtractAssociatedIcon(icon_path).ToBitmap();
-                            Icon_Control icon_box = new Icon_Control(img, icon_name, x, y);
-                            Controls.Add(icon_box);
-                            y += 50;
-                            if (y >= 500)
-                            {
-                                x += 50;
-                                y = 0;
-                            }
-                        }
-                        catch (Exception ex)
-                        {
-                            //MessageBox.Show(ex.Message);
-                            string new_icon_path = icon_path.Remove(icon_path.Length - 2);
-                            try
-                            {
-                                Image img = Icon.ExtractAssociatedIcon(new_icon_path).ToBitmap();
-                                Icon_Control icon_box = new Icon_Control(img, icon_name, x, y);
-                                Controls.Add(icon_box);
-                                y += 50;
-                                if (y >= 500)
-                                {
-                                    x += 50;
-                                    y = 0;
-                                }
-                            }
-                            catch (Exception ex1)
-                            {
-                                //MessageBox.Show(ex1.Message);
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        //public  async void ProcessMonitor()
+        //{
+        //    await Task.Run(async () => {
+        //        //using (RegistryKey reg_key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"))
+        //        using (RegistryKey reg_key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"))
+        //        {
+        //            string[] arr_app_names = reg_key.GetSubKeyNames();
+        //            foreach (var item_app_name in arr_app_names)
+        //            {
+        //                using (RegistryKey app_key = reg_key.OpenSubKey(item_app_name))
+        //                {
+        //                    string display_name = app_key?.GetValue("DisplayName")?.ToString();
+        //                    string app_name = app_key?.GetValue("DisplayIcon")?.ToString();
+        //                    if (app_name != null && display_name != null && app_name.Contains(".exe"))
+        //                    {
+        //                        string process_name = Regex.Match(app_name, @".*\\(.*)\.exe").Groups[1].Value;
+        //                        foreach (var item in Process.GetProcessesByName(process_name))
+        //                        {
+        //                            if (!list_names.Contains(display_name)) list_names.Add(display_name);
+        //                            //Console.WriteLine(display_name);
+        //                        }
+        //                    }
+        //                }
+        //            }
+        //            list_names.ForEach(Console.WriteLine);
+
+        //            listBox_ProgramsRun.Items.AddRange(list_names.ToArray());
+        //            //desktop.ShowDialog();
+        //        }
+        //        await Task.Delay(1);
+        //    });
+
+        //}
+
+
+
+
+
+
+
+
+
+        //private void Get_Icons()
+        //{
+        //    int x = 0, y = 0;
+        //    using (RegistryKey reg_key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"))
+        //    //using (RegistryKey reg_key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"))
+        //    {
+        //        string[] app_names = reg_key.GetSubKeyNames();
+        //        foreach (string name in app_names)
+        //        {
+
+        //            string icon_path = null;
+        //            string icon_name = null;
+        //            using (RegistryKey app_key = reg_key.OpenSubKey(name))
+        //            {
+        //                icon_path = app_key?.GetValue("DisplayIcon")?.ToString();
+        //                icon_name = app_key?.GetValue("DisplayName")?.ToString();
+        //            }
+        //            if (icon_path != null)
+        //            {
+        //                try
+        //                {
+        //                    Image img = Icon.ExtractAssociatedIcon(icon_path).ToBitmap();
+        //                    Icon_Control icon_box = new Icon_Control(img, icon_name, x, y);
+        //                    Controls.Add(icon_box);
+        //                    y += 50;
+        //                    if (y >= 500)
+        //                    {
+        //                        x += 50;
+        //                        y = 0;
+        //                    }
+        //                }
+        //                catch (Exception ex)
+        //                {
+        //                    //MessageBox.Show(ex.Message);
+        //                    string new_icon_path = icon_path.Remove(icon_path.Length - 2);
+        //                    try
+        //                    {
+        //                        Image img = Icon.ExtractAssociatedIcon(new_icon_path).ToBitmap();
+        //                        Icon_Control icon_box = new Icon_Control(img, icon_name, x, y);
+        //                        Controls.Add(icon_box);
+        //                        y += 50;
+        //                        if (y >= 500)
+        //                        {
+        //                            x += 50;
+        //                            y = 0;
+        //                        }
+        //                    }
+        //                    catch (Exception ex1)
+        //                    {
+        //                        //MessageBox.Show(ex1.Message);
+        //                    }
+        //                }
+        //            }
+        //        }
+        //    }
+        //}
         private void Form1_ControlRemoved(object sender, ControlEventArgs e)
         {
             if (Controls.Count == 0)
--- ParrentConsole/Program.cs
+++ ParrentConsole/Program.cs
@@ -9,63 +9,93 @@ using System.Text.RegularExpressions;
 //using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Threading;
 //using System.Timers;
 
 namespace ParrentConsole
 {
-    internal class Program
+   public class Program2
     {
         static List<string> list_names = new List<string>();
         static Desktop desktop = new Desktop();
         //static Timer timer = new Timer();
         static void Main(string[] args)
         {
+            //DispatcherTimer timer = new DispatcherTimer();
             //Timer timer = new Timer();
-            //timer.Interval = 10;
-            //timer.Tick += ((s, e) => ProcessMonitor());
+            //timer.Interval = TimeSpan.FromSeconds(1);
+           // timer.Tick += ((s, e) => ProcessMonitor());
             //timer.Start();
-            ProcessMonitor();
+
+            //desktop.timer.Tick+= ((s, e) => ProcessMonitor());
+            //desktop.timer.Start();
+
+            //ProcessMonitor();
             ShowDisplay();
             ProcessMonitor();  
+
+            //TimerStart();
+           // ShowDisplay();
             Console.ReadLine();
         }
-        static async void ShowDisplay()
+        //static async void TimerStart()
+        //{
+        //    await Task.Run(async () =>
+        //    {
+        //        Timer timer = new Timer();
+        //        timer.Interval = 100;
+        //        timer.Tick += ((s, e) => ProcessMonitor());
+        //        timer.Start();
+        //        await Task.Delay(1);
+        //    });
+        //}
+
+        static void ShowDisplay()
         {
-            await Task.Run(async () => {
-                desktop.ShowDialog(); 
-                await Task.Delay(1); 
+             Task.Run(() => {
+                desktop.ShowDialog();  
             });
         }
-        static async void ProcessMonitor()
+        public static void ProcessMonitor()
         {
-            await Task.Run(async() => {
+             Task.Run(async() => {
                 //using (RegistryKey reg_key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"))
                 using (RegistryKey reg_key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"))
                 {
                     string[] arr_app_names = reg_key.GetSubKeyNames();
-                    foreach (var item_app_name in arr_app_names)
+                    while (true)
                     {
-                        using (RegistryKey app_key = reg_key.OpenSubKey(item_app_name))
+                        foreach (var item_app_name in arr_app_names)
                         {
-                            string display_name = app_key?.GetValue("DisplayName")?.ToString();
-                            string app_name = app_key?.GetValue("DisplayIcon")?.ToString();
-                            if (app_name != null && display_name != null && app_name.Contains(".exe"))
+                            using (RegistryKey app_key = reg_key.OpenSubKey(item_app_name))
                             {
-                                string process_name = Regex.Match(app_name, @".*\\(.*)\.exe").Groups[1].Value;
-                                foreach (var item in Process.GetProcessesByName(process_name))
+                                string display_name = app_key?.GetValue("DisplayName")?.ToString();
+                                string app_name = app_key?.GetValue("DisplayIcon")?.ToString();
+                                if (app_name != null && display_name != null && app_name.Contains(".exe"))
                                 {
-                                    if (!list_names.Contains(display_name)) list_names.Add(display_name);
-                                    //Console.WriteLine(display_name);
+                                    string process_name = Regex.Match(app_name, @".*\\(.*)\.exe").Groups[1].Value;
+                                    foreach (var item in Process.GetProcessesByName(process_name))
+                                    {
+                                        if (!list_names.Contains(display_name)) 
+                                        {
+                                            list_names.Add(display_name);
+                                            desktop.listBox_ProgramsRun.Items.Add(display_name);
+                                            Console.WriteLine(display_name);
+                                        }
+                                        //Console.WriteLine(display_name);
+                                    }
                                 }
                             }
                         }
-                    }
-                    list_names.ForEach(Console.WriteLine);
+                        //list_names.ForEach(Console.WriteLine);
 
-                    desktop.listBox_ProgramsRun.Items.AddRange(list_names.ToArray());
+                        //desktop.listBox_ProgramsRun.Items.AddRange(list_names.ToArray());
+                        await Task.Delay(1000);
+                    }
+                    
                     //desktop.ShowDialog();
                 }
-                await Task.Delay(1);
+                
             });
             
         }
